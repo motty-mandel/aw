f5dea666c4a1cb1319f9e23ebfc3e65b3a01a82d
motty mandel <mandelmottyisrael65@gmail.com>
2024-08-27 20:35:13 -0400

    fixed axios get request

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     useEffect(() => {
         const fetchPaintings = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/api/paintings');
+                const response = await axios.get('https://aw-qgd0.onrender.com/api/paintings');
                 setPaintingsList(response.data)
             } catch (error) {
                 console.error('Error fetching paintings', error);
