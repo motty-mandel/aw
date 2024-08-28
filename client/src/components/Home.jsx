7e947cb40510c842ae1a1cfc697fcd177adec2b3
motty mandel <mandelmottyisrael65@gmail.com>
2024-08-27 20:51:50 -0400

    changed axios get request to ip address

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     useEffect(() => {
         const fetchPaintings = async () => {
             try {
-                const response = await axios.get('https://aw-qgd0.onrender.com/api/paintings');
+                const response = await axios.get('http://192.168.1.162:5000/api/paintings');
                 setPaintingsList(response.data)
             } catch (error) {
                 console.error('Error fetching paintings', error);
