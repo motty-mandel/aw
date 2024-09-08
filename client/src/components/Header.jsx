4eff970564c4f0eb9a58af21e1262973a992b3df
motty mandel <mandelmottyisrael65@gmail.com>
2024-09-08 00:57:26 -0400

    more style changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,6 @@ export default function Header() {
 
     const [theme, setTheme] = useState(localStorage.getItem('theme') || 'Light');
     const [btnName, setBtnName] = useState(localStorage.getItem('themeBtn') || 'Dark');
-    const [menuVisible, setMenuVisible] = useState(false);
 
     const toggleTheme = () => {
         if (theme === 'Light') {
@@ -33,50 +32,11 @@ export default function Header() {
         document.body.className = theme;
     }, [theme]);
 
-    const menu = (e) => {
-        e.stopPropagation();
-        setMenuVisible(!menuVisible);
-        let header = document.getElementById('header');
-        if (!menuVisible) {
-            header.style.width = '90%';
-        } else {
-            header.style.width = '100%';
-        }
-    };
-
-    const resetMenu = () => {
-        setMenuVisible(false);
-        let header = document.getElementById('header');
-        header.style.width = '100%';
-    };
-
-    useEffect(() => {
-        if (menuVisible) {
-            document.addEventListener('click', resetMenu);
-        } else {
-            document.removeEventListener('click', resetMenu);
-        }
-        return () => {
-            document.removeEventListener('click', resetMenu);
-        }
-    }, [menuVisible]);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
     return (
         <div class={`App-${theme}`}>
             <div class="main-man d-flex align-items-center justify-content-between">
 
-                <header class="header d-flex flex-row align-items-center justify-content-between p-3" id="header">
+                <header class="header d-flex flex-row align-items-center justify-content-between p-3">
 
                     <div class="profile left-nav">
                         <img src={avigailProfile} alt="profile-pic" height={'150px'} />
@@ -87,15 +47,16 @@ export default function Header() {
                         <img src={lightBorderPic} alt="flower-border" height={"70px"} />
                     </nav>
 
-                    <div class="right-nav d-flex align-items-center" id="right-nav">
+                    <div class="right-nav d-flex align-items-center">
                         <div class="btn-group dropleft">
                             <button type="button" class="btn-md btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 MENU
                             </button>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
+                                <Link to={'/'}><p>Home</p></Link>
+                                <Link to={'/connect'}><p>Connect</p></Link>
+                                <Link to={'/purchases'}><p>Purchase</p></Link>
+                                <p onClick={toggleTheme}>{btnName} mode</p>
                             </div>
                         </div>
                     </div>
