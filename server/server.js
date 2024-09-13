c717ce58f754e0214a6d6a72145d820f4cf30a7b
motty mandel <mandelmottyisrael65@gmail.com>
2024-09-13 17:45:40 -0400

    stripe stuff

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const stripe = require('stripe')(process.env.STRIPE_SECRET1);
+const stripe = require('stripe')("process.env.STRIPE_SECRET1");
 
 const app = express();
 app.use(cors());
