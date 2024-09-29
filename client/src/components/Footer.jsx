4a053d04114fd8c659442f6f7017fa1e6de70534
motty mandel <mandelmottyisrael65@gmail.com>
2024-09-29 19:43:03 -0400

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -36,7 +36,7 @@ export default function Footer() {
                         {/* <!-- Grid row --> */}
                         <div class="row mt-3">
                             {/* <!-- Grid column --> */}
-                            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <div class="col-md-5 col-lg-4 col-xl-5 mx-auto mb-4">
                                 {/* <!-- Content --> */}
                                 <h6 class="text-uppercase fw-bold mb-4">
                                     <i class="fas fa-gem me-3"></i>Avigails Art
@@ -49,50 +49,7 @@ export default function Footer() {
                                     always a way to express it, you just have to find your way.
                                 </p>
                             </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 products">
-                                {/* <!-- Links --> */}
-                                <h6 class="text-uppercase fw-bold mb-4">
-                                    Products
-                                </h6>
-                                <p>
-                                    <a href="#!" class="text-reset">Angular</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">React</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">Vue</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">Laravel</a>
-                                </p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 useful-links">
-                                {/* <!-- Links --> */}
-                                <h6 class="text-uppercase fw-bold mb-4">
-                                    Useful links
-                                </h6>
-                                <p>
-                                    <a href="#!" class="text-reset">Pricing</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">Settings</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">Orders</a>
-                                </p>
-                                <p>
-                                    <a href="#!" class="text-reset">Help</a>
-                                </p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-
+                            
                             {/* <!-- Grid column --> */}
                             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                                 {/* <!-- Links --> */}
