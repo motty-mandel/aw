26898772ee948059ee1385e9dc63525c9d7c351e
motty mandel <mandelmottyisrael65@gmail.com>
2024-10-02 14:11:18 -0400

    trying new temp addon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -56,6 +56,7 @@ export default function Header() {
                                 <Link to={'/'}><p>Home</p></Link>
                                 <Link to={'/sets'}><p>Painting sets</p></Link>
                                 <Link to={'/connect'}><p>Connect</p></Link>
+                                <Link to={'/temp'}><p style={{ display: 'none' }}>hi</p></Link>
                                 {/* <Link to={'/purchases'}><p>Purchase</p></Link> */}
                                 <p onClick={toggleTheme}>{btnName} mode</p>
                             </div>
